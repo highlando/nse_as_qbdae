38859f2fdd4a7e0e171b1bf318ff05b2a44ebad9
highlando <jnhlnd@gmail.com>
2014-04-08 15:16:35 +0200

    works

--- assemble_nse_qbdaearrays.py
+++ assemble_nse_qbdaearrays.py
@@ -4,6 +4,7 @@ import numpy as np
 from dolfin import dx, grad, inner
 
 import dolfin_navier_scipy as dns
+import dolfin_navier_scipy.dolfin_to_sparrays as dnsts
 
 dolfin.parameters.linear_algebra_backend = "uBLAS"
 
@@ -11,17 +12,25 @@ dolfin.parameters.linear_algebra_backend = "uBLAS"
 def test_qbdae_ass():
     femp, stokesmatsc, rhsd_vfrc, rhsd_stbc, \
         data_prfx, ddir, proutdir = \
-        dns.problem_setups.get_sysmats(problem='drivencavity', N=10, nu=1e-2)
+        dns.problem_setups.get_sysmats(problem='drivencavity', N=25, nu=1e-2)
 
     invinds = femp['invinds']
-    ass_convmat_asmatquad(V=femp['V'], invinds=invinds)
+    # nv = femp['V'].dim()
+    # invinds = np.arange(nv)
+
+    ass_convmat_asmatquad(W=femp['V'], invindsw=invinds)
 
 
-def ass_convmat_asmatquad(W=None, invinds=None):
+def ass_convmat_asmatquad(W=None, invindsw=None):
+    """ assemble the convection matrix H, so that N(v)v = H[v.v]
+
+    """
     mesh = W.mesh()
 
     V = dolfin.FunctionSpace(mesh, 'CG', 2)
-    # W = dolfin.VectorFunctionSpace(mesh, 'CG', 2)
+
+    # this is very specific for V being a 2D VectorFunctionSpace
+    invindsv = invindsw[::2]/2
 
     v = dolfin.TrialFunction(V)
     vt = dolfin.TestFunction(V)
@@ -55,7 +64,8 @@ def ass_convmat_asmatquad(W=None, invinds=None):
         return xm + ym
 
     nklist = []
-    for i in range(V.dim()):
+    for i in invindsv:
+    # for i in range(V.dim()):
         # iterate for the columns
         bi = dolfin.Function(V)
         bvec = np.zeros((V.dim(), ))
@@ -72,6 +82,7 @@ def ass_convmat_asmatquad(W=None, invinds=None):
         nyim = sps.csr_matrix((values, cols, rows))
 
         nxyim = _shuff_mrg_csrmats(nxim, nyim)
+        nxyim = nxyim[invindsv, :][:, invindsw]
         nyxxim = _pad_csrmats_wzerorows(nxyim.copy(), wheretoput='after')
         nyxyim = _pad_csrmats_wzerorows(nxyim.copy(), wheretoput='before')
 
@@ -79,47 +90,45 @@ def ass_convmat_asmatquad(W=None, invinds=None):
 
     hmat = sps.hstack(nklist, format='csc')
 
-    xexp = 'x[0]*x[0]*x[1]+1'
-    yexp = 'x[0]*x[0]*x[1]*x[1]'
+    xexp = '(1-x[0])*x[0]*(1-x[1])*x[1]*x[0]*0'
+    yexp = '(1-x[0])*x[0]*(1-x[1])*x[1]*x[1]+1'
+    # yexp = 'x[0]*x[0]*x[1]*x[1]'
 
     f = dolfin.Expression((xexp, yexp))
-    # fx = dolfin.Expression(xexp)
-    # fy = dolfin.Expression(yexp)
 
     u = dolfin.interpolate(f, W)
-    # ux = dolfin.interpolate(fx, V)
-    # uy = dolfin.interpolate(fy, V)
+    uvec = np.atleast_2d(u.vector().array()).T
+
+    uvec_gamma = uvec.copy()
+    uvec_gamma[invindsw] = 0
+    u_gamma = dolfin.Function(W)
+    u_gamma.vector().set_local(uvec_gamma)
+
+    uvec_i = 0*uvec
+    uvec_i[invindsw, :] = uvec[invindsw]
+    u_i = dolfin.Function(W)
+    u_i.vector().set_local(uvec_i)
 
     # Assemble the 'actual' form
     nform = dolfin.assemble(inner(grad(w) * u, wt) * dx)
     rows, cols, values = nform.data()
     nmat = sps.csr_matrix((values, cols, rows))
+    nmatrc = nmat[invindsw, :][:, :]
+    # nmatrccc = nmatrc[:, :][:, invindsw]
+    # nmatc = nmat[invindsw, :][:, :]
 
-    uvec = np.atleast_2d(u.vector().array()).T
-    # uvecx = np.atleast_2d(ux.vector().array()).T
-    # uvecy = np.atleast_2d(uy.vector().array()).T
-    # uvecxy = np.vstack([uvecx, uvecy])
+    N1, N2, fv = dnsts.get_convmats(u0_dolfun=u_gamma, V=W)
 
-    print np.linalg.norm(nmat * uvec)
-    print np.linalg.norm(hmat * np.kron(uvec, uvec))
-    # print np.linalg.norm(nmat*uvec - hmat*np.kron(uvecxy, uvecxy))
-
-    # wxinds = W.sub(0).dofmap().dofs()
-    # wyinds = W.sub(1).dofmap().dofs()
-
-    # shifwinds = np.r_[wxinds, wyinds]
-
-    # hmat_rowswpdt = col_columns_atend(hmat.T, wyinds)
-    # nmat_rowswpdt = col_columns_atend(nmat.T, wyinds)
-
-    # print np.linalg.norm(hmat_rowswpdt.T*np.kron(uvecxy, uvecxy))
-
-    # print np.c_[hmat*np.kron(uvecxy, uvecxy),
-    #             nmat_rowswpdt.T*uvec,
-    #             nmat*uvec,
-    #             hmat_rowswpdt.T*np.kron(uvecxy, uvecxy)]
-
-    # print np.linalg.norm(uvec[shifwinds] - uvecxy)
+    # print np.linalg.norm(nmatc * uvec[invindsw])
+    print np.linalg.norm(nmatrc * uvec)
+    print np.linalg.norm(nmatrc * uvec_i + nmatrc * uvec_gamma)
+    print np.linalg.norm(hmat * np.kron(uvec[invindsw], uvec[invindsw])
+                         + ((N1+N2)*uvec_i)[invindsw, :] + fv[invindsw, :])
+    # print np.linalg.norm((hmat * np.kron(uvec, uvec))[invindsw, :])
+    # print np.linalg.norm(((N1+N2)*uvec)[invindsw, :] + fv[invindsw, :])
+    print 'consistency tests'
+    print np.linalg.norm(uvec[invindsw]) - np.linalg.norm(uvec_i)
+    print np.linalg.norm(uvec - uvec_gamma - uvec_i)
 
 
 if __name__ == '__main__':
